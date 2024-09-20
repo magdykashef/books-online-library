bc4b811d9eb8122455562ca3c628e5fd541d7710
magdykashef <m.khashef@mindtorches.com>
2024-09-21 01:22:07 +0300

    update author models

--- src/app/shared/models/author.model.ts
+++ src/app/shared/models/author.model.ts
@@ -1,12 +1,27 @@
-import { Created } from "./created.model";
-import { Type } from "./type.model";
+import { Type } from "./type.model"
+export interface AuthorDetails{
+  numFound:      number;
+  start:         number;
+  numFoundExact: boolean;
+  docs:          Doc[];
+}
 
+export interface Doc {
+  alternate_names?: string[];
+  key:              string;
+  name:             string;
+  top_subjects?:    string[];
+  top_work:         string;
+  type:             Type;
+  work_count:       number;
+  _version_:        number;
+  date?:            string;
+  birth_date?:      string;
+  death_date?:      string;
+}
 export interface Author {
-  name:            string;
-  key:             string;
-  type:            Type;
-  latest_revision: number;
-  revision:        number;
-  created:         Created;
-  last_modified:   Created;
+  name:   string;
+  key:   string;
+  type:   Type;
+  author: Type;
 }
