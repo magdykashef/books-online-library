7b0a1bb660b086018feb67f46ca16a80082d57c3
magdykashef <m.khashef@mindtorches.com>
2024-09-20 16:44:30 +0300

    install and configure required libs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAnimations(),
+    provideAnimationsAsync(),
+    importProvidersFrom([
+      BrowserModule,
+      NgxSpinnerModule,
+    ]),
+    provideToastr({
+      positionClass: 'toast-top-center',
+      closeButton: true,
+    }),
+  ]
 };
