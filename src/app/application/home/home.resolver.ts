ee3710db7d429676db8ccb26459f8fbc7cbfe261
magdykashef <m.khashef@mindtorches.com>
2024-09-20 23:12:44 +0300

    moved service to shared for reusability

--- src/app/application/home/home.resolver.ts
+++ src/app/application/home/home.resolver.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { forkJoin, Observable } from 'rxjs';
-import { SubjectService } from './services/subject.service';
+import { SubjectService } from '../../shared/services/subject.service';
 import { SubjectResponse } from './models/subject-response.model';
 
 export const homeResolver: ResolveFn<Observable<{ subjectRes: SubjectResponse }>> = (route, state) => {
--- src/app/shared/services/subject.service.ts
+++ src/app/shared/services/subject.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { SubjectResponse } from '../models/subject-response.model';
+import { SubjectResponse } from '../../application/home/models/subject-response.model';
 import { map, Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: "root" })
 export class SubjectService {
