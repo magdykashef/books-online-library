99b8c305297862b6aee998586a164100774828cb
magdykashef <m.khashef@mindtorches.com>
2024-09-21 01:23:12 +0300

    bind author details data

--- src/app/application/author-details/author-details.component.ts
+++ src/app/application/author-details/author-details.component.ts
@@ -1,13 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe, TitleCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthorDetails } from '../../shared/models/author.model';
 
 @Component({
   selector: 'app-author-details',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe, DatePipe],
   templateUrl: './author-details.component.html',
   styleUrl: './author-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthorDetailsComponent {
-
+  readonly authorDetails = signal<AuthorDetails>(inject(ActivatedRoute).snapshot.data['data']['authorDetails']);
 }
