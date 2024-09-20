3010efd829c6d99c78cbb426df2eb4d7485d83b7
magdykashef <m.khashef@mindtorches.com>
2024-09-20 20:51:36 +0300

    apply home-page UI and bind Data

--- src/app/application/home/home.component.ts
+++ src/app/application/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { SubjectResponse } from './models/subject-response.model';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-
+  private readonly route: ActivatedRoute = inject(ActivatedRoute)
+  readonly subjectResponse = signal<Partial<SubjectResponse> | null>(this.route.snapshot.data['data']['subjectRes']);
 }
