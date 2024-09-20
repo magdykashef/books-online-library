c1b4fb1989e0907481dd9a91bba0508c967a68cd
magdykashef <m.khashef@mindtorches.com>
2024-09-20 18:23:54 +0300

    user header component

--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../core/components/header/header.component";
 
 @Component({
   selector: 'app-application',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   providers: [],
   templateUrl: './application.component.html',
   styleUrl: './application.component.scss'
