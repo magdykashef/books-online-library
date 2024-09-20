0df874dae506e8db695a7293055365e2b9069f23
magdykashef <m.khashef@mindtorches.com>
2024-09-20 17:13:33 +0300

    configure application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full'
+  },
+  {
+    path: 'app',
+    loadChildren: () => import('./application/application.routes')
+  },
+  {
+    path: '**',
+    redirectTo: 'app',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/application/application.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { ApplicationComponent } from './application.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ApplicationComponent,
+    },
+];
+
+export default routes;
